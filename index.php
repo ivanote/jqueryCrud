12f6c3f704405e9a2a5af133487e9d1b1f001364
ivanote <ivangallegovela@gmail.com>
2018-06-29 12:47:06 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP and MySQL CRUD Operations Demo</title>
+
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+
+
+</head>
+<body>
+
+<!-- Content Section -->
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<h2>PHP and MySQL CRUD Operations</h2>
+<div class="pull-right">
+
+<button class="btn btn-success" data-toggle="modal" data-target="#add_new_record_modal">Add New Record</button>
+
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-md-12">
+<h4>Records:</h4>
+<div class="records_content"></div>
+</div>
+</div>
+</div>
+<!-- /Content Section -->
+
+
+
+<!-- Bootstrap Modal - To Add New Record -->
+<!-- Modal -->
+<div class="modal fade" id="add_new_record_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal-dialog" role="document">
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+<h4 class="modal-title" id="myModalLabel">Add New Record</h4>
+</div>
+<div class="modal-body">
+
+<div class="form-group">
+<label for="first_name">First Name</label>
+<input type="text" id="first_name" placeholder="First Name" class="form-control" />
+</div>
+
+<div class="form-group">
+<label for="last_name">Last Name</label>
+<input type="text" id="last_name" placeholder="Last Name" class="form-control" />
+</div>
+
+<div class="form-group">
+<label for="email">Email Address</label>
+<input type="text" id="email" placeholder="Email Address" class="form-control" />
+</div>
+
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+<button type="button" class="btn btn-primary" onclick="addRecord()">Add Record</button>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+
+<!-- Modal - Update User details -->
+<div class="modal fade" id="update_user_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Update</h4>
+            </div>
+            <div class="modal-body">
+
+                <div class="form-group">
+                    <label for="update_first_name">First Name</label>
+                    <input type="text" id="update_first_name" placeholder="First Name" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                    <label for="update_last_name">Last Name</label>
+                    <input type="text" id="update_last_name" placeholder="Last Name" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                    <label for="update_email">Email Address</label>
+                    <input type="text" id="update_email" placeholder="Email Address" class="form-control"/>
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary" onclick="UpdateUserDetails()" >Save Changes</button>
+                <input type="hidden" id="hidden_user_id">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- // Modal -->
+
+
+<!-- Custom JS file -->
+<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
